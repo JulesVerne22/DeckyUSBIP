4c6fafe7400fafa211b8b388f08a65c9efc78827
Julian Smith <juliari114@gmail.com>
2025-12-28 17:31:58 -0600

    feat: added code to plugin

--- main.py
+++ main.py
@@ -3,131 +3,99 @@ import logging
 from os import path
 from settings import SettingsManager
 from helpers import get_user
-import re
 
 USER = get_user()
 HOME_PATH = "/home/" + USER
 HOMEBREW_PATH = HOME_PATH + "/homebrew"
 
-logging.basicConfig(filename="/tmp/tunneldeck.log",
-                    format='[TunnelDeck] %(asctime)s %(levelname)s %(message)s',
+logging.basicConfig(filename="/tmp/deckyusbip.log",
+                    format='[DeckyUSBIP] %(asctime)s %(levelname)s %(message)s',
                     filemode='w+',
                     force=True)
-logger=logging.getLogger()
+logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
-def connection_mapper(xn):
-    components = re.split(r'\s{2,}', xn)
+
+def device_mapper(xn):
+    components = xn.split("\n")
     return {
-        "name": components[0],
-        "uuid": components[1],
-        "type": components[2],
-        "device": components[3],
-        "connected": False if components[3] == '--' else True
+        "id": components[0].split(' ')[3],
+        "name": components[1].lstrip(),
     }
 
-def get_active_connection():
-    result = subprocess.run(["nmcli", "connection", "show", "--active"], text=True, capture_output=True).stdout
-    connections = result.splitlines()
-    connections.pop(0)
-    mapped = map(connection_mapper, connections)
-    return next(filter(lambda xn: xn["type"] == 'wifi' or xn["type"] == 'ethernet', mapped), None)
+
+def get_available_devices():
+    result = subprocess.run(["usbip", "list", "-l"], text=True, capture_output=True).stdout
+    devices = result.split("\n\n")
+    devices.pop(-1)
+    mapped = map(device_mapper, devices)
+    return next(mapped, None)
+
 
 def run_install_script():
     logger.info("Running Install Script")
     subprocess.run(["bash", path.dirname(__file__) + "/extensions/install"], cwd=path.dirname(__file__) + "/extensions")
 
+
 def run_uninstall_script():
     logger.info("Running Uninstall Script")
     subprocess.run(["bash", path.dirname(__file__) + "/extensions/uninstall"], cwd=path.dirname(__file__) + "/extensions")
 
+
 class Plugin:
 
-    settings: SettingsManager = SettingsManager("tunneldeck", path.join(HOMEBREW_PATH, "settings"))
+    settings: SettingsManager = SettingsManager("deckyusbip", path.join(HOMEBREW_PATH, "settings"))
 
     async def _main(self):
-        logger.info("Loading OpenVPN setting")
-        openvpn_enabled = self.settings.getSetting("openvpn_enabled", False)
-        if openvpn_enabled:
-            logger.info("OpenVPN enabled: " + "yes" if openvpn_enabled else "no")
+        logger.info("Running install script if not installed")
+        installed = self.settings.getSetting("installed", False)
+        if not installed:
+            logger.info("Not installed, running install script")
             run_install_script()
 
-    # Lists the connections from network manager.
-    # If device is -- then it's disconnected.
+    # Lists the usb devices from usbip.
     async def show(self):
-        result = subprocess.run(["nmcli", "connection", "show"], text=True, capture_output=True).stdout
-        connections = result.splitlines()
-        connections.pop(0)        
-        mapped = map(connection_mapper, connections)
+        result = subprocess.run(["usbip", "list", "-l"], text=True, capture_output=True).stdout
+        devices = result.split("\n\n")
+        devices.pop(-1)
+        mapped = map(device_mapper, devices)
         return list(mapped)
 
-    # Establishes a connection to a VPN
-    async def up(self, uuid):
-        logger.info("OPENING connection to: " + uuid)
-        result = subprocess.run(["nmcli", "connection", "up", uuid], text=True, capture_output=True).stdout
+    # Binds usb device
+    async def up(self, id):
+        logger.info("Binding device: " + id)
+        result = subprocess.run(["usbip", "bind", "-b", id], text=True, capture_output=True).stdout
         return result
 
-    # Closes a connection to a VPN
-    async def down(self, uuid):
-        logger.info("CLOSING connection to: " + uuid)
-        result = subprocess.run(["nmcli", "connection", "down", uuid], text=True, capture_output=True).stdout
+    # Unbinds usb device
+    async def down(self, id):
+        logger.info("Unbinding device: " + id)
+        result = subprocess.run(["usbip", "unbind", "-b", id], text=True, capture_output=True).stdout
         return result
 
-    # Checks if IPV6 is disabled on Wi-Fi
-    async def active_connection(self):
-        connection = get_active_connection()
-        if connection == None:
-            return None
-
-        result = subprocess.run(["nmcli", "connection", "show", connection["uuid"], "|", "grep", "ipv6.method"], text=True, capture_output=True).stdout
-        connection["ipv6_disabled"] = True if "disabled" in result else False
-        return connection
-
-    # Disables IPV6 on currently active connection
-    async def disable_ipv6(self):
-        connection = get_active_connection()
-        if connection == None:
-            return True
-        
-        logger.info("DISABLING IPV6 for: " + connection["uuid"])
-        subprocess.run(["nmcli", "connection", "modify", connection["uuid"], "ipv6.method", "disabled"])
-        subprocess.run(["systemctl", "restart", "NetworkManager"])
-        return True
-
-    # Enable IPV6 on currently active connection
-    async def enable_ipv6(self):
-        connection = get_active_connection()
-        if connection == None:
-            return True
-        
-        logger.info("ENABLING IPV6 for: " + connection["uuid"])
-        subprocess.run(["nmcli", "connection", "modify", connection["uuid"], "ipv6.method", "auto"])
-        subprocess.run(["systemctl", "restart", "NetworkManager"])
-        return True
-
-    # Checks if the OpenVPN package is installed
-    async def is_openvpn_pacman_installed(self):
+    # Checks if usbip is installed
+    async def is_usbip_installed(self):
         try:
-            subprocess.run(["pacman", "-Qi", "networkmanager-openvpn"], check=True)
+            subprocess.run(["usbip", "list", "-l"], check=True)
             return True
         except subprocess.CalledProcessError:
             return False
 
-    # The OpenVPN setting
-    async def is_openvpn_enabled(self):
-        return self.settings.getSetting("openvpn_enabled", False)
+    # The installed setting
+    async def is_plugin_installed(self):
+        return self.settings.getSetting("installed", False)
 
-    # Enable OpenVPN
-    async def enable_openvpn(self):
-        logger.info("Enabling OpenVPN")
-        self.settings.setSetting("openvpn_enabled", True)
+    # Install plugin
+    async def install_plugin(self):
+        logger.info("Running install script...")
+        self.settings.setSetting("installed", True)
         run_install_script()
         return True
 
-    # Disable OpenVPN
-    async def disable_openvpn(self):
-        logger.info("Disabling OpenVPN")
-        self.settings.setSetting("openvpn_enabled", False)
+    # Uninstall plugin
+    async def uninstall_plugin(self):
+        logger.info("Running uninstall script...")
+        self.settings.setSetting("installed", False)
         run_uninstall_script()
         return True
 
--- src/index.tsx
+++ src/index.tsx
@@ -4,9 +4,7 @@ import {
   PanelSectionRow,
   ServerAPI,
   staticClasses,
-  ToggleField,
   ButtonItem,
-  Navigation
 } from "decky-frontend-lib";
 
 import {
@@ -17,62 +15,22 @@ import {
 
 import { FaShieldAlt } from "react-icons/fa";
 
-type Connection = {
+type Device = {
+  id: string,
   name: string,
-  uuid: string,
-  type: string,
-  connected: boolean,
-  ipv6_disabled?: boolean
 }
 
 const Content: VFC<{ serverAPI: ServerAPI }> = ({ serverAPI }) => {
 
   const [ loaded, setLoaded ] = useState(false);
-  const [ connections, setConnections ] = useState<Connection[]>([]);
-  const [ activeConnection, setActiveConnection ] = useState<Connection>();
-  const [ ipv6Disabled, setIpv6Disabled ] = useState(false);
-  const [ openVPNEnabled, setOpenVPNEnabled ] = useState(false);
-  const [ openVPNDisabled, setOpenVPNDisabled ] = useState(false);
+  const [ devices, setDevices ] = useState<Device[]>([]);
 
-  const loadConnections = async () => {
-
+  const loadDevices = async () => {
     try {
-      const activeConnectionResponse = await serverAPI.callPluginMethod<{}, Connection>('active_connection', {});
-      const activeConnection = activeConnectionResponse.result as Connection;
-      setActiveConnection(activeConnection);
-      setIpv6Disabled((activeConnection.ipv6_disabled) ? true : false);
-    } catch (error) {
-      console.error(error);
-    }
+      const response = await serverAPI.callPluginMethod<{}, Device[]>('show', {});
+      const devices = response.result as Device[];
 
-    try {
-      const openVPNDisabled = await serverAPI.callPluginMethod<{}, boolean>('is_openvpn_pacman_installed', {});
-      setOpenVPNDisabled(openVPNDisabled.result as boolean);
-    } catch (error) {
-      console.error(error);
-    }
-
-    if(!openVPNDisabled) {
-      try {
-        const openVPNEnabledResponse = await serverAPI.callPluginMethod<{}, boolean>('is_openvpn_enabled', {});
-        setOpenVPNEnabled(openVPNEnabledResponse.result as boolean);
-      } catch (error) {
-        console.error(error);
-      }
-    }
-
-    try {
-      const response = await serverAPI.callPluginMethod<{}, Connection[]>('show', {});
-      const connections = response.result as Connection[];
-      const filtered = connections
-      .filter((connection) => ['vpn', 'wireguard'].includes(connection.type))
-      .sort((a, b) => {
-        if(a.name < b.name) return -1;
-        if(a.name > b.name) return 1;
-        return 0;
-      });
-
-      setConnections(filtered);
+      setDevices(devices);
     } catch (error) {
       console.error(error);
     }
@@ -80,75 +38,48 @@ const Content: VFC<{ serverAPI: ServerAPI }> = ({ serverAPI }) => {
     setLoaded(true);
   }
 
-  const toggleConnection = async (connection: Connection, switchValue: boolean) => {
-    await serverAPI.callPluginMethod((switchValue) ? 'up' : 'down', { uuid: connection.uuid });
-  }
-
-  const toggleIpv6 = async(switchValue: boolean) => {
-    setIpv6Disabled(switchValue);
-    await serverAPI.callPluginMethod((switchValue) ? 'disable_ipv6' : 'enable_ipv6', {});
-  }
-
-  const toggleOpenVPN = async(switchValue: boolean) => {
-    setOpenVPNEnabled(switchValue);
-    await serverAPI.callPluginMethod((switchValue) ? 'enable_openvpn' : 'disable_openvpn', {});
+  const toggleBind = async (device: Device, switchValue: boolean) => {
+    await serverAPI.callPluginMethod((switchValue) ? 'up' : 'down', { id: device.id });
   }
 
   useEffect(() => {
-    loadConnections();
+    loadDevices();
   }, []);
 
   return (
     <>
-      <PanelSection title="Connections">
+      <PanelSection title="Devices">
 
-        {loaded && connections.length == 0 && <PanelSectionRow>
-          No Connections Found
+        {loaded && devices.length == 0 && <PanelSectionRow>
+          No Devices Found
         </PanelSectionRow>}
 
-        {connections.length > 0 && connections.map((connection) => (
+        {devices.length > 0 && devices.map((device) => (
           <PanelSectionRow>
-            <ToggleField
-            bottomSeparator='standard'
-            checked={connection.connected}
-            label={connection.name}
-            description={`Type: ${connection.type}`}
-            onChange={(switchValue: boolean) => {
-              toggleConnection(connection, switchValue);
-            }} />
+            {device.name}
+            <ButtonItem onClick={() => {
+              toggleBind(device, true)
+            }}>
+            Bind {device.id}
+            </ButtonItem>
+            <ButtonItem onClick={() => {
+              toggleBind(device, false)
+            }}>
+            Unbind {device.id}
+            </ButtonItem>
           </PanelSectionRow>
         ))}
-
-        <PanelSectionRow>
-          <ButtonItem onClick={() => {
-            Navigation.NavigateToExternalWeb('https://github.com/steve228uk/TunnelDeck#readme');
-            Navigation.CloseSideMenus();
-          }}>
-            How Do I Add Connections?
-          </ButtonItem>
-        </PanelSectionRow>
 
       </PanelSection>
       <PanelSection title="Settings">
         <PanelSectionRow>
-          <ToggleField
-          bottomSeparator='standard'
-          checked={openVPNEnabled || openVPNDisabled}
-          label='Enable OpenVPN'
-          disabled={!loaded || openVPNDisabled}
-          description='Installs OpenVPN support for Network Manager'
-          onChange={toggleOpenVPN} />
+          <ButtonItem onClick={() => {
+            loadDevices()
+          }}>
+          Reload USB devices
+          </ButtonItem>
         </PanelSectionRow>
 
-        <PanelSectionRow>
-          <ToggleField
-          bottomSeparator='standard'
-          checked={ipv6Disabled}
-          label='Disable IPV6'
-          disabled={!activeConnection || !loaded}
-          description='Disables IPV6 support for the current connection. Required for some VPNs.'
-          onChange={toggleIpv6} />
-        </PanelSectionRow>
       </PanelSection>
     </>
   );
@@ -156,7 +87,7 @@ const Content: VFC<{ serverAPI: ServerAPI }> = ({ serverAPI }) => {
 
 export default definePlugin((serverApi: ServerAPI) => {
   return {
-    title: <div className={staticClasses.Title}>TunnelDeck</div>,
+    title: <div className={staticClasses.Title}>DeckyUSBIP</div>,
     content: <Content serverAPI={serverApi} />,
     icon: <FaShieldAlt />,
   };
